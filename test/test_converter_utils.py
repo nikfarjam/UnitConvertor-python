d12e6ef9c5741672729642aeba3f3d3300c642ee
nikfarjam <mm.nikfarjam@gmail.com>
2020-03-22 20:17:37 +1100

    Use pytest to test

--- test/test_converter_utils.py
+++ test/test_converter_utils.py
@@ -1,11 +1,10 @@
-import unittest
+import pytest
 from converter_utils import celsius_to_fahrenheit
 
 
-class TestUnitConverter(unittest.TestCase):
+def test_celsius_to_fahrenheit_100():
+    assert celsius_to_fahrenheit(100) == 212
 
-    def test_celsius_to_fahrenheit_100(self):
-        self.assertEqual(celsius_to_fahrenheit(100), 212)
 
-    def test_celsius_to_fahrenheit_with_default_rounding(self):
-        self.assertEqual(celsius_to_fahrenheit(1), 33.8)
+def test_celsius_to_fahrenheit_with_default_rounding():
+    assert celsius_to_fahrenheit(1) == 33.8
--- test/test_math_utils.py
+++ test/test_math_utils.py
@@ -1,26 +1,29 @@
-import unittest
+import pytest
 from math_utils import is_float
 
+def test_is_float_none_value():
+    assert not is_float(None)
 
-class TestMathUtil(unittest.TestCase):
 
-    def test_is_float_None_value(self):
-        self.assertFalse(is_float(None))
+def test_is_float_empty_value():
+    assert not is_float('')
 
-    def test_is_float_empty_value(self):
-        self.assertFalse(is_float(''))
 
-    def test_is_float_alphabetic_value(self):
-        self.assertFalse(is_float('abc'))
+def test_is_float_alphabetic_value():
+    assert not is_float('abc')
 
-    def test_is_float_mix_value(self):
-        self.assertFalse(is_float('12A'))
 
-    def test_is_float_digit_value(self):
-        self.assertTrue(is_float('12'))
+def test_is_float_mix_value():
+    assert not is_float('12A')
 
-    def test_is_float_float_value(self):
-        self.assertTrue(is_float('12.234'))
 
-    def test_is_float_invalid_value(self):
-        self.assertFalse(is_float('12.234.45'))
+def test_is_float_digit_value():
+    assert is_float('12')
+
+
+def test_is_float_float_value():
+    assert is_float('12.234')
+
+
+def test_is_float_invalid_value():
+    assert not is_float('12.234.45')
